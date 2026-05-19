c72418a2e4482690d063b0b9656ef1919109f81f
marissah2327240151 <marissahanggitaputri_2327240151@mhs.mdp.ac.id>
2026-05-19 15:21:31 +0700

    key

--- notes/web/firebase-messaging-sw.js
+++ notes/web/firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 // Gunakan konfigurasi web dari firebase_options.dart Anda
 firebase.initializeApp({
-  apiKey: "AIzaSyAN...",
-  authDomain: "notes-5be35.firebaseapp.com",
-  projectId: "notes-5be35",
-  storageBucket: "notes-5be35.firebasestorage.app",
-  messagingSenderId: "1014103626721",
-  appId: "1:1014103626721:web:f3b42914...",
+  apiKey: "AIzaSyC4F_drrXwW4ElYiw9Fd_AP_bEFkiy4XbU",
+  authDomain: "notes-f0a92.firebaseapp.com",
+  projectId: "notes-f0a92",
+  storageBucket: "notes-f0a92.firebasestorage.app",
+  messagingSenderId: "191611950738",
+  appId: "1:191611950738:web:ff1e7799a5e85b4e0fb84b",
 });
 
 const messaging = firebase.messaging();
